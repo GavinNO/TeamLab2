4c6ebe5ec2f23c4aebf0a4581322e7cd18e98e42
Breckin1027 <BrLuke01@wsc.edu>
2025-10-03 12:36:41 -0500

    Accepted my merge

--- TeamLab2/src/components/AddTaskForm.jsx
+++ TeamLab2/src/components/AddTaskForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 
-<<<<<<< HEAD
 function AddTaskForm({ onAdd }) {
   const [name, setName] = useState("");
 
@@ -25,34 +24,3 @@ function AddTaskForm({ onAdd }) {
     </form>
   );
 }
-=======
-export default function AddTaskForm({ onAction }) {  
-  const [name, setName] = useState(""); // controlled input
-
-  function addTask() {
-    if (name.length >= 3) {
-      onAction(name);
-      setName(""); // clear after submit
-    }
-  }
-
-  return (
-    <div style={{ marginTop: "1rem" }}>
-      <input 
-        type="text" 
-        placeholder="Enter task name" 
-        value={name}
-        onChange={(e) => setName(e.target.value)}
-      />
-      <button 
-        onClick={addTask} 
-        disabled={name.length < 3} // disable until ≥3 chars
-      >
-        Create
-      </button>
-    </div>
-  );
-}
-
-
->>>>>>> c72cf1cfaf565784aeb1c74aee1f94ee9f13cecb
